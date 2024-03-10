07ea78fb4a46b0d41c831afad9f3e750b00fac8b
DengYangyong <dyyjxcjdx@163.com>
2024-03-10 15:24:11 -0400

    Load retrieval train data

---
+++ code/retrieval/data_loader.py
@@ -0,0 +1,61 @@
+import json
+import os
+import pandas as pd
+
+
+def load_dataset():
+
+    data_dir = "../../data/sci_retriever"
+    # load dataframe
+    query_df = pd.read_parquet(os.path.join(data_dir, "sci_queries.parquet"))
+    content_df = pd.read_parquet(os.path.join(data_dir, "sci_contents.parquet"))
+    labels_df = pd.read_parquet(os.path.join(data_dir, "sci_labels.parquet"))
+    fold_df = pd.read_parquet(os.path.join(data_dir, "sci_folds.parquet"))
+
+    # split data into train, val
+    is_train = fold_df['is_train'] == 1
+    labels_df_train = labels_df[labels_df["query_id"].isin(fold_df[is_train]["query_id"])]
+    labels_df_val = labels_df[labels_df["query_id"].isin(fold_df[~is_train]["query_id"])]
+
+    query_df_train = query_df[query_df["query_id"].isin(labels_df_train["query_id"])]
+    query_df_val = query_df[query_df["query_id"].isin(labels_df_val["query_id"])]
+
+    content_df_train = content_df[content_df["content_id"].isin(labels_df_train["content_id"])]
+    content_df_val = content_df[content_df["content_id"].isin(labels_df_val["content_id"])]
+
+    # convert data to json
+    train_json = data_to_json(query_df_train, content_df_train, labels_df_train)
+    val_json = data_to_json(query_df_val, content_df_val, labels_df_val)
+
+    # save json
+    with open(os.path.join(data_dir, "train_dataset.json"), "w") as f:
+        json.dump(train_json, f)
+
+    with open(os.path.join(data_dir, "val_dataset.json"), "w") as f:
+        json.dump(val_json, f)
+
+
+def data_to_json(query_df, content_df, labels_df):
+
+    id2queries = dict(
+        zip(query_df['query_id'], query_df[["prompt", "A", "B", "C", "D", "E"]].apply(lambda x: " | ".join(x), axis=1))
+    )
+
+    id2content = dict(
+        zip(content_df['content_id'], content_df['context'])
+    )
+
+    # format: {"query_id": [content_id, ...]}
+    relevant_docs = labels_df.groupby('query_id')['content_id'].apply(list).to_dict()
+
+    json_data = {
+        "queries": id2queries,
+        "corpus": id2content,
+        "relevant_docs": relevant_docs,
+        "mode": "text"
+    }
+
+    return json_data
+
+if __name__ == "__main__":
+    load_dataset()
