99a4fb0938921b203dece417a7b15e8b9f91b3cd
DengYangyong <dyyjxcjdx@163.com>
2024-03-24 18:44:22 -0400

    Update deepspeed train shell

--- train_llm_deepspeed.sh
+++ train_llm_deepspeed.sh
@@ -1,2 +1,3 @@
 export HYDRA_FULL_ERROR=1
+python code/classifier/data_loader.py
 nohup accelerate launch --config_file "config/accelerate/accelerate_config.yaml" code/classifier/train_llm.py > llm.log 2>&1 &
